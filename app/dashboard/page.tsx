2f905359950a05335641591122adfab8e570ad91
Fatimakhan01 <fatimayousaf597@gmail.com>
2026-03-01 01:14:21 +0500

    Add task table component with layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import DashboardHeader from "@/components/dashboard/dashboard-header";
+import TaskTable from "@/components/dashboard/task-table";
 
 export default function DashboardPage() {
   return (
     <div>
       <DashboardHeader />
+      <TaskTable/>
       
     </div>
   );
